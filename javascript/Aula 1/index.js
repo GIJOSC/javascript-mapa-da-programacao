f7120b91a575a3bcf5eb40c2b7dddd63208d38e7
Gesiel Neves <neves.gesiel@gmail.com>
2023-12-02 16:08:58 -0300

    Add file .gitignore

--- javascript/Aula 1/index.js
+++ javascript/Aula 1/index.js
@@ -3,10 +3,15 @@ const prompt = require('prompt-sync')();
 let nome = prompt('Qual é o seu nome? ');
 let idade = prompt("Qual é a sua idade: ");
 let contratado = false;
+let pagamento = null;
+let cargo;
 
 
+console.log("====================================")
 console.log("Dados da ficha: ")
 console.log("====================================")
 console.log("Nome do funcionário: ",nome);
 console.log("Idade: ", idade);
 console.log("Já foi contratado: ", contratado);
+console.log("Pagamento a receber: ", pagamento);
+console.log("Cargo: ", cargo);
