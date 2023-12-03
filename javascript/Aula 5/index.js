7d13c9c1c3c7d4b4b70b2d5941f21add08e0976d
Gesiel Neves <neves.gesiel@gmail.com>
2023-12-02 22:57:08 -0300

    feat: Add switch case to Aula 5

--- javascript/Aula 5/index.js
+++ javascript/Aula 5/index.js
@@ -7,15 +7,39 @@ const prompt = require('prompt-sync')();
 //     console.log("Bom Jogador");
 // }
 
-let notaExatas = Number(prompt("Digite sua nota de exatas: "));
-let notaHumanas = Number(prompt("Digite sua nota de humanas: "));
-let notaRedacao = Number(prompt("Digite sua nota da redação: "));
+// let notaExatas = Number(prompt("Digite sua nota de exatas: "));
+// let notaHumanas = Number(prompt("Digite sua nota de humanas: "));
+// let notaRedacao = Number(prompt("Digite sua nota da redação: "));
 
-let media = (notaExatas + notaHumanas + notaRedacao) / 3;
-console.log("Média: ",media);
+// let media = (notaExatas + notaHumanas + notaRedacao) / 3;
+// console.log("Média: ",media);
 
-if(media >= 650) {
-    console.log("Boas chances de aprovação")
-} else {
-    console.log("Aprovação mais dificil");
+// if(media >= 650) {
+//     console.log("Boas chances de aprovação")
+// } else {
+//     console.log("Aprovação mais dificil");
+// }
+
+let nomeSelecao = prompt("Digite o nome da seleção: ");
+
+switch (nomeSelecao) {
+    case "brasil": {
+        console.log("Maior campeão");
+        break
+    }
+    case "italia": {
+        console.log("4 titulos");
+        break
+    }
+    case "alemanha": {
+        console.log("3 titulos");
+        break
+    }
+    case "argentina": {
+        console.log("La mano de Dios");
+        break
+    }
+    default: {
+        console.log("Seleção não encontrada!");
+    }
 }
